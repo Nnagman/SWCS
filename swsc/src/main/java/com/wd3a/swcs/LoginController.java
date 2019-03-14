db63097e78cc0f686cd0f5c7bf35e7233198218f
Nangman <ckdgh930314@naver.com>
2019-03-14 18:03:58 +0900

    원본

--- swsc/src/main/java/com/wd3a/swcs/LoginController.java
+++ swsc/src/main/java/com/wd3a/swcs/LoginController.java
@@ -11,29 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.wd3a.swcs.domain.MemberVO;
-import com.wd3a.swcs.service.MemberService;
-
 @Controller
 public class LoginController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
-	private MemberService memberService;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
 		
-		model.addAttribute("list", memberService.getList());
-		
 		return "login";
-	}
-	
-	@RequestMapping(value = "/logincheck", method = RequestMethod.GET)
-	public ModelAndView loginCheck(@ModelAttribute MemberVO vo, HttpSession session) {
-		boolean result = MemberService.loginCheck(vo, session);
-		return null;
-		
 	}
 	
 }
